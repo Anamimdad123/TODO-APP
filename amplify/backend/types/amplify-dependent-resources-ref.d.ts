3d1ba26948f004b3ff905f0efbf2f9b522322d7d
Anamimdad <144683993+Anamimdad@users.noreply.github.com>
2026-01-30 23:18:07 +0500

    sync amplify backend main env

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "reactcognito79d1f44f": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
